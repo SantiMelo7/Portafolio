36860dbaaaad12f17652bcc1328527c4cb346a47
Santiago Melo <santiagocano15cr7@gmail.com>
2024-01-24 21:08:31 -0600

    agregando componentes y svg

---
+++ src/components/Header/InfoMe.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Me from "../../../public/me.png"
+import AboutMe from "./AboutMe";
+import { GitHub, Linkedin, Email } from "@/svg/IconsSvg";
+
+export default function InfoMe() {
+
+    return (
+
+        <>
+
+            <section className="flex justify-center lg:mt-10 lg:flex-row sm:flex-col sm:mt-5">
+            
+                <AboutMe></AboutMe>
+ 
+                <div className="flex justify-center items-center lg:pl-10 lg:mt-1 sm:mt-4">
+ 
+                    <Image src={Me} className="rounded-md" alt="Santi" style={{width: 247, height: "auto"}}></Image>
+ 
+                </div>
+
+                <GitHub></GitHub>
+ 
+            </section>
+        
+        </>
+        
+    )
+
+}
