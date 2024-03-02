18fa7a5c080bbf50e4dc339317a23b049d819d13
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-01 18:59:40 -0600

    Temas de evitar mas etiquetas de html

--- src/components/Technologies/SectionTecnologies.jsx
+++ src/components/Technologies/SectionTecnologies.jsx
@@ -1,39 +1,66 @@
-import { Javascript, HTML, Postman, CssSvg, ReactJs, NextJs, MongoDB, Git, MaterialUi, GitHubSvg, Talwind, Vite,  } from "@/svg/IconsSvg";
+import {
+  Javascript,
+  HTML,
+  Postman,
+  CssSvg,
+  ReactJs,
+  NextJs,
+  MongoDB,
+  Git,
+  MaterialUi,
+  GitHubSvg,
+  Talwind,
+  Vite,
+  Axios,
+  StyledComponents,
+} from "@/svg/IconsSvg";
 import DivTec from "./DivTec";
 
 export default function SectionTechnologies() {
-
-    return (
-        
-        <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 xl:max-w-screen-xl md:max-w-screen-md sm:max-w-screen-sm sm:ml-10 gap-10 mt-10">
-
-            <DivTec><HTML></HTML></DivTec>
-            
-            <DivTec><CssSvg></CssSvg></DivTec>
-
-            <DivTec><Javascript></Javascript></DivTec>
-
-            <DivTec><ReactJs></ReactJs></DivTec>
-
-            <DivTec><NextJs></NextJs></DivTec>
-
-            <DivTec><Talwind></Talwind></DivTec>
-            
-            <DivTec><Git></Git></DivTec>
-
-            <DivTec><GitHubSvg></GitHubSvg></DivTec>
-
-            <DivTec><MongoDB></MongoDB></DivTec>
-            
-            <DivTec><Postman></Postman></DivTec>
-
-            <DivTec><Vite></Vite></DivTec>
-
-            <DivTec><MaterialUi></MaterialUi></DivTec>
-
-        </div>
-        
-    )
-
-
+  return (
+    <div className="grid xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 xl:max-w-screen-xl md:max-w-screen-md sm:max-w-screen-sm sm:ml-10 gap-10 mt-10">
+      <DivTec>
+        <HTML />
+      </DivTec>
+      <DivTec>
+        <CssSvg />
+      </DivTec>
+      <DivTec>
+        <Javascript />
+      </DivTec>
+      <DivTec>
+        <ReactJs />
+      </DivTec>
+      <DivTec>
+        <NextJs />
+      </DivTec>
+      <DivTec>
+        <Talwind />
+      </DivTec>
+      <DivTec>
+        <StyledComponents />
+      </DivTec>
+      <DivTec>
+        <Git />
+      </DivTec>
+      <DivTec>
+        <GitHubSvg />
+      </DivTec>
+      <DivTec>
+        <MongoDB />
+      </DivTec>
+      <DivTec>
+        <Postman />
+      </DivTec>
+      <DivTec>
+        <Axios />
+      </DivTec>
+      <DivTec>
+        <Vite />
+      </DivTec>
+      <DivTec>
+        <MaterialUi />
+      </DivTec>
+    </div>
+  );
 }
--- src/components/Technologies/Technology.jsx
+++ src/components/Technologies/Technology.jsx
@@ -2,21 +2,12 @@ import SectionTechnologies from "./SectionTecnologies";
 import Title from "./Title";
 
 export default function Technology() {
-
-    return (
-    
-        <>
-        
-            <Title title="Technologies"></Title>
-
-            <div className="flex flex-col max-w-5xl mx-auto">
-
-                <SectionTechnologies></SectionTechnologies>
-
-            </div>
-
-        </>
-
-    )
-
+  return (
+    <>
+      <Title title="Technologies" />
+      <div className="flex flex-col max-w-5xl mx-auto">
+        <SectionTechnologies />
+      </div>
+    </>
+  );
 }
