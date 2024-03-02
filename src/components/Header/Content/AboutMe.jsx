f30780b32fc3e3a962080769d0d0e6876dd340ee
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-01 19:01:27 -0600

    Título

--- src/components/Header/Content/AboutMe.jsx
+++ src/components/Header/Content/AboutMe.jsx
@@ -1,33 +1,21 @@
+import TitleInitial from "@/components/layout/TitleInitial";
 import IconsLink from "../Icons/IconsLink";
 import ImageHeader from "./Image";
 import Paragrafh from "./Paragrafh";
 
-export default function AboutMe () {
-  
-    return (
-
-        <>
-
-            <div className="flex flex-col xl:mt-28 absolute sm:justify-center xl:max-w-2xl sm:max-w-screen-md lg:text-start sm:text-center sm:ml-4">
-                
-                <ImageHeader></ImageHeader>
-
-                <div className="flex flex-col xl:ml-8 gap-4 mt-5 sm:p-2">
-
-                    <h1 className="text-5xl font-bold text-primary">Hi, I'm Santiago Melo</h1>
-
-                    <Paragrafh></Paragrafh>
-
-                    <nav className="flex gap-x-10 xl:justify-center xl:items-center xl:flex-row sm:flex-col md:flex-row sm:items-center">
-                    <IconsLink></IconsLink>
-                    </nav>
-
-                </div>
-
-            </div>
-        
-        </>
-
-    )
-
+export default function AboutMe() {
+  return (
+    <>
+      <div className="flex flex-col xl:mt-28 absolute sm:justify-center xl:max-w-2xl sm:max-w-screen-md lg:text-start sm:text-center sm:ml-4">
+        <ImageHeader />
+        <div className="flex flex-col xl:ml-8 gap-4 mt-5 sm:p-2">
+          <TitleInitial text="Hi, I'm Santiago Melo" className="text-5xl" />
+          <Paragrafh />
+          <nav className="flex gap-x-10 xl:justify-center xl:items-center xl:flex-row sm:flex-col md:flex-row sm:items-center">
+            <IconsLink />
+          </nav>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/layout/TitleInitial.jsx
@@ -0,0 +1,3 @@
+export default function TitleInitial({ text, className }) {
+  return <h1 className={`${className} font-bold text-primary`}>{text}</h1>;
+}
