6ce969b7e8a821ad739a7d9e1e97dc87fa64db1c
Santiago Melo <santiagocano15cr7@gmail.com>
2024-03-01 19:01:12 -0600

    Links

--- src/components/Header/Icons/IconsLink.jsx
+++ src/components/Header/Icons/IconsLink.jsx
@@ -2,25 +2,24 @@ import Links from "./Links";
 import { GitHub, Linkedin } from "@/svg/IconsSvg";
 
 export default function IconsLink() {
-
-    return (
-        
-        <>
-
-            <Links href={"https://github.com/SantiMelo7"}>
-    
-                <GitHub></GitHub> GitHub
-
-            </Links>
-
-            <Links href={"https://www.linkedin.com/in/santiago-melo-7b25732a5/"}>
-            
-                <Linkedin></Linkedin> Linkedin
-
-            </Links>
-        
-        </>
-        
-    )
-
+  return (
+    <>
+      <Links
+        className="border-[3px] border-cyan-500 rounded-xl shadow-xl shadow-gray-500 flex justify-center items-center text-xl text-[#00FFF0] 
+        transition md:hover:scale-110 hover:bg-white/30 sm:w-52 md:gap-x-4 md:mt-1 sm:px-12 sm:gap-x-4 sm:mt-4 sm:hover:scale-95"
+        href={"https://github.com/SantiMelo7"}
+        text="GitHub"
+      >
+        <GitHub />
+      </Links>
+      <Links
+        className="border-[3px] border-cyan-500 rounded-xl shadow-xl shadow-gray-500 flex justify-center items-center text-xl text-[#00FFF0] 
+        transition md:hover:scale-110 hover:bg-white/30 sm:w-52 md:gap-x-4 md:mt-1 sm:px-12 sm:gap-x-4 sm:mt-4 sm:hover:scale-95"
+        href={"https://www.linkedin.com/in/santiago-melo-7b25732a5/"}
+        text="Linkedin"
+      >
+        <Linkedin />
+      </Links>
+    </>
+  );
 }
--- src/components/Header/Icons/Links.jsx
+++ src/components/Header/Icons/Links.jsx
@@ -1,19 +1,14 @@
-export default function Links({href, children, disabled}) {
-
-    return ( // md:text-basetext-black/70 md:py-2 md:px-4
-        
-        <a disabled={disabled} href={href} className="border-[3px] border-cyan-500 rounded-xl 
-        
-            shadow-xl shadow-gray-500 flex justify-center items-center text-xl text-[#00FFF0]
-            
-            transition md:hover:scale-110 hover:bg-white/30 sm:w-52 
-            
-            md:gap-x-4 md:mt-1 sm:px-12 sm:gap-x-4 sm:mt-4 sm:hover:scale-95"
-
-            target="_blank" rel="noopener noreferrer"
-
-        > {children} </a>
-        
-    )
-
+export default function Links({ href, className, children, disabled, text }) {
+  return (
+    <a
+      disabled={disabled}
+      href={href}
+      className={className}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {text}
+      {children}
+    </a>
+  );
 }
