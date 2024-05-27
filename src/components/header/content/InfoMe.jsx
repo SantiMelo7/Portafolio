45f08d3354784c17be888feb86f53e54a040ef88
Santiago Melo <santiagocano15cr7@gmail.com>
2024-05-26 21:08:13 -0600

    changes

--- src/components/header/content/InfoMe.jsx
+++ src/components/header/content/InfoMe.jsx
@@ -2,7 +2,7 @@ import SubText from "./SubText";
 
 export default function InfoMe() {
   return (
-    <section className="lg:grid lg:grid-cols-2 max-w-screen-2xl mx-auto lg:h-[80vh] md:h-[70vh] sm:h-[125vh]">
+    <section className="lg:grid lg:grid-cols-2 max-w-screen-2xl mx-auto lg:h-[80vh] md:h-[40vh] sm:h-[103vh]">
       <div className="flex flex-row lg:justify-start sm:justify-center ">
         <SubText />
       </div>
