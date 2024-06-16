8ceed0de85095ec0fd78ddac210e94249906fb05
Santiago Melo <santiagocano15cr7@gmail.com>
2024-06-16 15:49:38 -0600

    responsive

--- src/components/header/icons/IconsLink.jsx
+++ src/components/header/icons/IconsLink.jsx
@@ -4,7 +4,7 @@ import { Curriculum, GitHub, Gmail, Linkedin } from "../../../svg/IconsSvg";
 export default function IconsLink() {
   return (
     <nav className="icons-link-perfil">
-      <div className="sm:flex gap-x-6 mx-auto">
+      <div className="md:flex gap-x-3 mx-auto">
         <Links
           className="w-56 cv"
           href={"https://github.com/SantiMelo7"}
@@ -27,7 +27,7 @@ export default function IconsLink() {
           <Gmail />
         </Links>
       </div>
-      <div className="flex md:justify-start sm:mb-16 md:mb-10 sm:justify-center">
+      <div className="flex xl:justify-start xl:-translate-x-0 md:translate-x-5 sm:justify-center">
         <Links
           className="md:w-52 sm:w-56 py-3 px-3 cv"
           href={"https://curriculum-dev.vercel.app/"}
